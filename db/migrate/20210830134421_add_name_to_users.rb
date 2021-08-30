1d2e9609330dcd2b41f92f6f8684d993dfb975e1
Samuel <samueltitterington@gmail.com>
2021-08-30 16:26:13 +0200

    Modified User table to take a first, last name and avatar

--- db/migrate/20210830134421_add_name_to_users.rb
+++ db/migrate/20210830134421_add_name_to_users.rb
@@ -1,5 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :avatar, :string, default: "https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,9 @@ ActiveRecord::Schema.define(version: 2021_08_30_135739) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar", default: "https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
