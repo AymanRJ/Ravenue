30e5eb0a30243f35416382bd75ce72843be0d44e
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2021-08-31 17:45:40 +0200

    designed index page, created venues in seed file for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "start seeding.. creates 6 venues"
+
+test_user = User.create!(email: "abccxc@ab.com", password: "password", first_name:"test_user", last_name:"test_last")
+
+6.times { |i|
+  Venue.create!(title: "venue_#{i}", location: "locationXYZ", capacity: 125, price: 500, user: test_user)
+}
+
+puts "seeding done"
+
