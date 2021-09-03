e4e2f2bec8c24ba5cea46deee3b27ca3a6d523dc
Samuel <samueltitterington@gmail.com>
2021-09-03 14:38:10 +0200

    I know, I know, it looks fucking amazing, you're welcome

---
+++ app/javascript/plugins/type_js.js
@@ -0,0 +1,22 @@
+import Typed from 'typed.js';
+
+const initTyped = () => {
+  if (document.querySelector('#home-page-title')) {
+    new Typed('#home-page-title', {
+      strings: ['<strong><em> AWAITS </em></strong>',
+                '<strong><em> IN AN ^200 APARTMENT </em></strong>',
+                '<strong><em> IN A ^200 GARDEN</em></strong>',
+                '<strong><em> IN A ^200 WAREHOUSE </em></strong>',
+                '<strong><em> WHEREVER </em></strong>',
+                '<strong><em> WHENEVER </em></strong>'],
+      loop: true,
+      startDelay: 500,
+      typeSpeed: 50,
+      smartBackspace: true,
+      backSpeed: 50,
+      showCursor: false
+    });
+  }   
+}
+
+export { initTyped };
